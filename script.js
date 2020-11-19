2488237e950821d658762f417c942bcd6b00aac9
bielRodriguesBatista <gabriel731rodrigues@gmail.com>
2020-11-19 05:50:02 -0300

    numberButton/JS

--- script.js
+++ script.js
@@ -73,6 +73,34 @@ class Calculator {
     } else {
       return integerDisplay
     }
+  }
 
+  updateDisplay(){
+    this.currentOperandTextElement.innerText = 
+    this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null){
+      this.previousOperandTextElement.innerText = 
+      `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    }
+    else {
+      this.previousOperandTextElement.innerText = ''
+    }
   }
 }
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperand(button.innerText)
+    calculator.updateDisplay()
+  })
+})
