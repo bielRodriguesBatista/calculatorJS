35bce84706fdc1fd0a658226037570cbaf8eb0f9
bielRodriguesBatista <gabriel731rodrigues@gmail.com>
2020-12-18 16:05:36 -0300

    upnumberButtonstApplyingUseStrict

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 class Calculator {
+  "use strict"
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement
     this.currentOperandTextElement = currentOperandTextElement
@@ -80,8 +81,24 @@ class Calculator {
     this.currentOperandTextElement.innerText =
       this.getDisplayNumber(this.currentOperand)
     if(this.operation != null){
-      this.previousOperandTextElement.innerText
-      
+      this.previousOperandTextElement.innerText = ''
     }
   }
 }
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('data-previous-operand')
+const currentOperandTextElement = document.querySelector('data-current-operand')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
